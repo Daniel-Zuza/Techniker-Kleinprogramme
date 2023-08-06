92f84c920284b9b27326e7f61ad7d87096b0d26a
Daniel <dani.zuza@gmx.de>
2023-08-06 11:39:50 +0200

    Bedienung von Punktzahlrechner geaendert

--- Punktzahlrechner/punktzahlrechner.py
+++ Punktzahlrechner/punktzahlrechner.py
@@ -1,63 +1,79 @@
 
+import os
+
 def gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert):
     # Gewichtungen errechnen basierend darauf wie aktuell eine Lernsession ist
-    # und dann alte Lernsessions auf Medianwerte setzen und laenge der Session je nach Gewichtung kürzen
+    # und dann alte Lernsessions auf Durchschnittswerte setzen und laenge der Session je nach Gewichtung kürzen
+
+    if len(themen_punkte) < 2:  # sicherstellen, dass mind. 2 Lernsession vorhanden sind
+
+        return sum(themen_punkte[0]) / len(themen_punkte[0])
+
     sessionanzahl = len(themen_punkte)
     gewichtete_themenpunkte = []
-    step = (1 - clippwert) / (sessionanzahl -1)
+    step = (1 - clippwert) / (sessionanzahl - 1)
     gewichtungen = [clippwert + step * i for i, gewicht in enumerate(themen_punkte)]
 
     for gewicht, punkte in zip(gewichtungen, themen_punkte):
-        median_sessionpunkte = sum(punkte) / len(punkte)
+        durchschnitt_sessionpunkte = sum(punkte) / len(punkte)
         punkte_gewichtungsgrenze = int(len(punkte) * gewicht)
 
         for i in range(punkte_gewichtungsgrenze):
-            gewichtete_themenpunkte.append(median_sessionpunkte)
+            gewichtete_themenpunkte.append(durchschnitt_sessionpunkte)
 
     return sum(gewichtete_themenpunkte) / len(gewichtete_themenpunkte)
 
 def punkteErrechnen(clippwert):
+    if os.path.exists('resultate.txt'):
+        os.remove('resultate.txt')
+
     with open('punktzahlen.txt', 'r') as file:
-        session_punkte = []
-        themen_punkte = []
-        themen_mittelwerte = []
-        themen_letztsession_mittelwerte = []
-        themenbereich = ''
+        with open('resultate.txt', 'w') as resultate:
+
+            session_punkte = []
+            themen_punkte = []
+            themen_mittelwerte = []
+            themen_letztsession_mittelwerte = []
+            themenbereich = ''
 
 
-        for line in file:
+            for line in file:
 
-            if '/' in line:
-                if themenbereich != '':
-                    themenmittelwert = gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert)
+                if '/' in line:
+                    if themenbereich != '':
+                        themenmittelwert = gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert)
 
-                    themen_mittelwerte.append(themenmittelwert)
-                    themen_letztsession_mittelwerte.append(ref_session_punktzahl)
-                    print(f'Themen Punktzahl von {themenbereich}: {themenmittelwert}\n')
+                        themen_mittelwerte.append(themenmittelwert)
+                        themen_letztsession_mittelwerte.append(ref_session_punktzahl)
+                        print(f'Themen Punktzahl von {themenbereich}: {themenmittelwert}\n')
+                        resultate.write(f'Themen Punktzahl von {themenbereich}: {themenmittelwert}\n')
 
-                    themen_punkte = []
+                        themen_punkte = []
 
-                themenbereich = ' '.join(line.strip().split(' ')[1:])
+                    themenbereich = ' '.join(line.strip().split(' ')[1:])
 
-            elif '#' in line:
-                if len(session_punkte) != 0:
-                    ref_sessionlaenge = len(session_punkte)
-                    ref_session_punktzahl = sum(session_punkte) / len(session_punkte)
-                    print(f'Session Punktzahl: {ref_session_punktzahl}')
-                    themen_punkte.append(session_punkte)
-                    session_punkte = []
+                elif '#' in line:
+                    if len(session_punkte) != 0:
+                        ref_session_punktzahl = sum(session_punkte) / len(session_punkte)
+                        #print(f'Session Punktzahl: {ref_session_punktzahl}')
+                        #resultate.write(f'Session Punktzahl: {ref_session_punktzahl}\n')
+                        themen_punkte.append(session_punkte)
+                        session_punkte = []
 
-            else:
-                for punkt in line.strip().split(', '):
-                    if not '-' in punkt:
-                        session_punkte.append(int(punkt))
+                else:
+                    for punkt in line.strip().split(', '):
+                        if not '-' in punkt:
+                            session_punkte.append(int(punkt))
 
-    if not len(themen_punkte) == 0:
-        print(f'Themen Punktzahl von {themenbereich}: {gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert)}\n')
-    if not len(themen_mittelwerte) == 0:
-        print(f'themenübergreifende Gesamte Punktzahl: {sum(themen_mittelwerte) / len(themen_mittelwerte)}')
-    if not len(themen_letztsession_mittelwerte) == 0:
-        print(f'themenübergreifende Letztsession Punktzahl: {sum(themen_letztsession_mittelwerte) / len(themen_letztsession_mittelwerte)}')
+            if not len(themen_punkte) == 0:
+                print(f'Themen Punktzahl von {themenbereich}: {gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert)}\n')
+                resultate.write(f'Themen Punktzahl von {themenbereich}: {gewichtetenThemenmittelwertErrechnen(themen_punkte, clippwert)}\n')
+            if not len(themen_mittelwerte) == 0:
+                print(f'themenübergreifende Gesamte Punktzahl: {sum(themen_mittelwerte) / len(themen_mittelwerte)}')
+                resultate.write(f'\nthemenübergreifende Gesamte Punktzahl: {sum(themen_mittelwerte) / len(themen_mittelwerte)}\n')
+            if not len(themen_letztsession_mittelwerte) == 0:
+                print(f'themenübergreifende Letztsession Punktzahl: {sum(themen_letztsession_mittelwerte) / len(themen_letztsession_mittelwerte)}')
+                resultate.write(f'themenübergreifende Letztsession Punktzahl: {sum(themen_letztsession_mittelwerte) / len(themen_letztsession_mittelwerte)}\n')
 
 # gewichtet
 punkteErrechnen(clippwert=0.25)
